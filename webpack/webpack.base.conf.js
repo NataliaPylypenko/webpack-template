9039e38e523c728a5385ae394ceb1ef998af6500
NataliaPilipenko <natalia28ivanivna@gmail.com>
2022-10-25 09:28:49 +0300

    choose color

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -21,7 +21,8 @@ module.exports = {
     // module: ['@babel/polyfill', `${PATHS.src}/js/your-module.js`],
     common: ['@babel/polyfill', `${PATHS.src}/js/common.js`], // general js
     home: ['@babel/polyfill', `${PATHS.src}/js/home.js`],
-    news: ['@babel/polyfill', `${PATHS.src}/js/news.js`]
+    news: ['@babel/polyfill', `${PATHS.src}/js/news.js`],
+    choose_colors: ['@babel/polyfill', `${PATHS.src}/js/choose_colors.js`]
   },
   output: {
     path: PATHS.web,
