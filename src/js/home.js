fe2390a5e38dcbf57ef1c1c99664d8f86c5b10ef
NataliaPylypenko <natalia28ivanivna@gmail.com>
2022-11-01 13:19:58 +0200

    added parallax

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,19 @@
 // SCSS
 import '../scss/home.scss'
+import gsap from "../js/libs/gsap/gsap.min";
+import ScrollTrigger from "../js/libs/gsap/ScrollTrigger.min";
+import ScrollSmoother from "../js/libs/gsap/ScrollSmoother.min";
 
 window.addEventListener('DOMContentLoaded', () => {});
+
+window.addEventListener('scroll', e => {
+    // document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
+    document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
